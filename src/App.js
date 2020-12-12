ce7b285db045de5ddb0502201f6344539f466ab9
unknown <ltatkhashvili@gmail.com>
2020-12-12 14:48:41 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [crystalbet, setCrystalbet] = useState();
+  const [adjarabet, setAdjarabet] = useState();
+  const [crystalbetMoney, setCrystalbetMoney] = useState();
+  const [adjarabetMoney, setAdjarabetMoney] = useState();
+  const [profit, setProfit] = useState();
+
+  const submit = async (event) => {
+    event.preventDefault();
+
+    let a = adjarabet;
+    let b = crystalbet;
+    let c = crystalbetMoney;
+    let z = (c * b) / (0.965 + (a - 1));
+
+    let prof = z - z * 0.035 - c;
+
+    setCrystalbet("");
+    setAdjarabet("");
+    setCrystalbetMoney("");
+    setAdjarabetMoney(z);
+    setProfit(prof);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="toto">
+      <form onSubmit={submit}>
+        <div>
+          Crystalbet Odd{" "}
+          <input
+            value={crystalbet}
+            required
+            onChange={({ target }) => setCrystalbet(target.value)}
+          />
+        </div>
+        <div>
+          Adjarabet Odd{" "}
+          <input
+            value={adjarabet}
+            required
+            onChange={({ target }) => setAdjarabet(target.value)}
+          />
+        </div>
+
+        <div>
+          Crystal Money{" "}
+          <input
+            value={crystalbetMoney}
+            required
+            onChange={({ target }) => setCrystalbetMoney(target.value)}
+          />
+        </div>
+
+        <button type="submit">Count</button>
+        <p>Adjarabet Money: {adjarabetMoney}</p>
+        <p>Profit: {profit}</p>
+      </form>
     </div>
   );
 }
