7e5ce811ad7c563d206b089ece73f810c95f7c91
unknown <ltatkhashvili@gmail.com>
2021-01-28 13:44:40 +0400

    second

--- src/App.js
+++ src/App.js
@@ -7,6 +7,12 @@ function App() {
   const [adjarabetMoney, setAdjarabetMoney] = useState();
   const [crys, setCrys] = useState();
   const [profit, setProfit] = useState();
+  const [crystalbet2, setCrystalbet2] = useState();
+  const [adjarabet2, setAdjarabet2] = useState();
+  const [crystalbetMoney2, setCrystalbetMoney2] = useState();
+  const [adjarabetMoney2, setAdjarabetMoney2] = useState();
+  const [crys2, setCrys2] = useState();
+  const [profit2, setProfit2] = useState();
 
   const submit = async (event) => {
     event.preventDefault();
@@ -21,6 +27,21 @@ function App() {
     setAdjarabetMoney(z.toFixed(1));
     setProfit(prof.toFixed(1));
     setCrys(c);
+  };
+
+  const submit2 = async (event) => {
+    event.preventDefault();
+
+    let a = adjarabet2;
+    let b = crystalbet2;
+    let c = crystalbetMoney2;
+    let z = (c * b) / (1 + (a - 1));
+
+    let prof = z - c;
+
+    setAdjarabetMoney2(z.toFixed(1));
+    setProfit2(prof.toFixed(1));
+    setCrys2(c);
   };
 
   return (
@@ -56,6 +77,39 @@ function App() {
         <p>Crystalbet Money: {crys}</p>
         <p>Adjarabet Money: {adjarabetMoney}</p>
         <p>Profit: {profit}</p>
+      </form>
+      <h2>Premier League</h2>
+      <form onSubmit={submit2}>
+        <div>
+          Crystalbet Odd{" "}
+          <input
+            value={crystalbet2}
+            required
+            onChange={({ target }) => setCrystalbet2(target.value)}
+          />
+        </div>
+        <div>
+          Adjarabet Odd{" "}
+          <input
+            value={adjarabet2}
+            required
+            onChange={({ target }) => setAdjarabet2(target.value)}
+          />
+        </div>
+
+        <div>
+          Crystal Money{" "}
+          <input
+            value={crystalbetMoney2}
+            required
+            onChange={({ target }) => setCrystalbetMoney2(target.value)}
+          />
+        </div>
+
+        <button type="submit">Count</button>
+        <p>Crystalbet Money: {crys2}</p>
+        <p>Adjarabet Money: {adjarabetMoney2}</p>
+        <p>Profit: {profit2}</p>
       </form>
     </div>
   );
